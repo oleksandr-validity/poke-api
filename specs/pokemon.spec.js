f8e75de1484d8b689b2f043f9a7a0b8e3d010532
Oleksandr Ostapchuk <sanyokthebest2@gmail.com>
2022-05-20 12:26:52 -0400

    2nd pokemon test

--- specs/pokemon.spec.js
+++ specs/pokemon.spec.js
@@ -8,30 +8,37 @@ let inputName = input[0].name;
 let inputMoves = input[0].moves;
 
 
-
-
 describe(`Pokemon: ${inputName}`,  () =>{
+  const request = supertest(process.env.BASE_URL)
+  let result;
   let pokemonMoves = [];
 
-  before(`Get all moves of Pokemon: ${inputName}`, () =>{
-    const request = supertest(process.env.BASE_URL)
-    request
+  before(async function(){
+    await request
       .get(`/pokemon/${inputName}`)
-      .end((err,res)=>{
-        pokemonMoves.push(res.body.moves[0])
-        console.log(pokemonMoves.length)
+      .then(res =>{
+        result = res;
       })
-  });
+  })
 
   it('Is pokemon name correct', function () {
-    const request = supertest(process.env.BASE_URL)
-    request
-      .get(`/pokemon/${inputName}`)
-      .end((err,res)=>{
-        expect(res.body.name).to.eq(inputName)
-      })
+    expect(result.body.name).to.eq(inputName)
+  })
 
+  it(`Get all moves of Pokemon: ${inputName}`, () =>{
+        for(let i  in result.body.moves){
+          pokemonMoves.push(result.body.moves[i].move.name)
+        }
+    console.log(pokemonMoves)
+  })
 
-  });
+  it('Check if move includes', ()=>{
+    for(let i in inputMoves){
+      if (pokemonMoves.includes(i)){
+        return true;
+      }
+      console.log(`${inputMoves[i]} = `)
+    }
+  })
 
 });
