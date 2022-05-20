206600be52b4c2eda4450790cc494d0bf2b56318
Oleksandr Ostapchuk <98861430+sanyokone@users.noreply.github.com>
2022-05-20 15:01:38 -0400

    test for api endpoint  /api/v2/pokemon/ (#4)
    * 2nd pokemon test
    
    * Pokemon test for api endpoint: /api/v2/pokemon/

--- helpers/checkMoves.js
+++
@@ -1,7 +0,0 @@
-// function checkPokemonMoves (pokemonMoves){
-//   for(let i = 0; i < pokemonMoves.length; i++){
-//     console.log(pokemonMoves[i].move.name);
-//   }
-// }
-//
-// export default checkPokemonMoves();
--- specs/pokemon.spec.js
+++ specs/pokemon.spec.js
@@ -1,37 +1,90 @@
 import supertest from 'supertest';
 import { expect }  from "chai";
 import 'dotenv/config'
-import * as input from './inputData.json'
-import checkMoves from "../helpers/checkMoves";
+import input from './dataset.json'
 
-let inputName = input[0].name;
-let inputMoves = input[0].moves;
+const request = supertest(process.env.BASE_URL)
+let pokemon = input[Math.floor(Math.random() * input.length)]
 
+let pokemonName = pokemon.name;
+let pokemonMoves = pokemon.moves;
+let apiResponse;
+let resPokemonMoves = [];
 
+describe(`Pokemon: ${pokemonName}`,  () =>{
 
-
-describe(`Pokemon: ${inputName}`,  () =>{
-  let pokemonMoves = [];
-
-  before(`Get all moves of Pokemon: ${inputName}`, () =>{
-    const request = supertest(process.env.BASE_URL)
-    request
-      .get(`/pokemon/${inputName}`)
-      .end((err,res)=>{
-        pokemonMoves.push(res.body.moves[0])
-        console.log(pokemonMoves.length)
+  before(async function(){
+    await request
+      .get(`/pokemon/${pokemonName}`)
+      .then(res =>{
+        apiResponse = res;
       })
-  });
+  })
 
-  it('Is pokemon name correct', function () {
-    const request = supertest(process.env.BASE_URL)
-    request
-      .get(`/pokemon/${inputName}`)
-      .end((err,res)=>{
-        expect(res.body.name).to.eq(inputName)
-      })
+  before(`Get all moves of Pokemon: ${pokemonName}`, () =>{
+        for(let i  in apiResponse.body.moves){
+          resPokemonMoves.push(apiResponse.body.moves[i].move.name)
+        }
+  })
 
+  it(`Check ${pokemonMoves[0]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[0])
+  })
 
-  });
+  it(`Check ${pokemonMoves[1]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[1])
+  })
+
+  it(`Check ${pokemonMoves[2]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[2])
+  })
+
+  it(`Check ${pokemonMoves[3]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[3])
+  })
+
+  it(`Check ${pokemonMoves[4]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[4])
+  })
+
+  it(`Check ${pokemonMoves[5]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[5])
+  })
+
+  it(`Check ${pokemonMoves[6]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[6])
+  })
+
+  it(`Check ${pokemonMoves[7]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[7])
+  })
+
+  it(`Check ${pokemonMoves[8]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[8])
+  })
+
+  it(`Check ${pokemonMoves[9]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[9])
+  })
+
+  it(`Check ${pokemonMoves[10]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[10])
+  })
+
+  it(`Check ${pokemonMoves[11]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[11])
+  })
+
+  it(`Check ${pokemonMoves[12]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[12])
+  })
+
+  it(`Check ${pokemonMoves[13]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[13])
+  })
+
+  it(`Check ${pokemonMoves[14]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[14])
+  })
 
 });
