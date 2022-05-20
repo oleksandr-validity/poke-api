40703151d84e0b466d9d3a5d2e47b3f3fade2bad
Oleksandr Ostapchuk <98861430+sanyokone@users.noreply.github.com>
2022-05-19 21:44:55 -0400

    first pokemon test (#3)

---
+++ helpers/checkMoves.js
@@ -0,0 +1,7 @@
+// function checkPokemonMoves (pokemonMoves){
+//   for(let i = 0; i < pokemonMoves.length; i++){
+//     console.log(pokemonMoves[i].move.name);
+//   }
+// }
+//
+// export default checkPokemonMoves();
