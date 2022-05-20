875491451825a60c7e2190d8de4d005eb9082f85
Oleksandr Ostapchuk <sanyokthebest2@gmail.com>
2022-05-20 14:59:51 -0400

    Pokemon test for api endpoint: /api/v2/pokemon/

--- specs/pokemon.spec.js
+++ specs/pokemon.spec.js
@@ -1,44 +1,90 @@
 import supertest from 'supertest';
 import { expect }  from "chai";
 import 'dotenv/config'
-import * as input from './inputData.json'
-import checkMoves from "../helpers/checkMoves";
+import input from './dataset.json'
 
-let inputName = input[0].name;
-let inputMoves = input[0].moves;
+const request = supertest(process.env.BASE_URL)
+let pokemon = input[Math.floor(Math.random() * input.length)]
 
+let pokemonName = pokemon.name;
+let pokemonMoves = pokemon.moves;
+let apiResponse;
+let resPokemonMoves = [];
 
-describe(`Pokemon: ${inputName}`,  () =>{
-  const request = supertest(process.env.BASE_URL)
-  let result;
-  let pokemonMoves = [];
+describe(`Pokemon: ${pokemonName}`,  () =>{
 
   before(async function(){
     await request
-      .get(`/pokemon/${inputName}`)
+      .get(`/pokemon/${pokemonName}`)
       .then(res =>{
-        result = res;
+        apiResponse = res;
       })
   })
 
-  it('Is pokemon name correct', function () {
-    expect(result.body.name).to.eq(inputName)
-  })
-
-  it(`Get all moves of Pokemon: ${inputName}`, () =>{
-        for(let i  in result.body.moves){
-          pokemonMoves.push(result.body.moves[i].move.name)
+  before(`Get all moves of Pokemon: ${pokemonName}`, () =>{
+        for(let i  in apiResponse.body.moves){
+          resPokemonMoves.push(apiResponse.body.moves[i].move.name)
         }
-    console.log(pokemonMoves)
   })
 
-  it('Check if move includes', ()=>{
-    for(let i in inputMoves){
-      if (pokemonMoves.includes(i)){
-        return true;
-      }
-      console.log(`${inputMoves[i]} = `)
-    }
+  it(`Check ${pokemonMoves[0]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[0])
+  })
+
+  it(`Check ${pokemonMoves[1]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[1])
+  })
+
+  it(`Check ${pokemonMoves[2]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[2])
+  })
+
+  it(`Check ${pokemonMoves[3]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[3])
+  })
+
+  it(`Check ${pokemonMoves[4]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[4])
+  })
+
+  it(`Check ${pokemonMoves[5]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[5])
+  })
+
+  it(`Check ${pokemonMoves[6]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[6])
+  })
+
+  it(`Check ${pokemonMoves[7]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[7])
+  })
+
+  it(`Check ${pokemonMoves[8]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[8])
+  })
+
+  it(`Check ${pokemonMoves[9]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[9])
+  })
+
+  it(`Check ${pokemonMoves[10]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[10])
+  })
+
+  it(`Check ${pokemonMoves[11]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[11])
+  })
+
+  it(`Check ${pokemonMoves[12]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[12])
+  })
+
+  it(`Check ${pokemonMoves[13]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[13])
+  })
+
+  it(`Check ${pokemonMoves[14]}`,()=>{
+    expect(resPokemonMoves).have.include(pokemonMoves[14])
   })
 
 });
